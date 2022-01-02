48090e25d217707a12d1ba82bb65a4bdb0431c34
wudilsr <wudilsr@gmail.com>
2022-01-02 08:16:23 +0800

    f460: New tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_f460.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_f460-user \
+    omni_f460-userdebug \
+    omni_f460-eng
